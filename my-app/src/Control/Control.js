13931df194633a734ef05bfa987ffa52f303e26d
Ying, Jingya <Jingya.Ying@sage.com>
2017-08-08 14:54:41 -0400

    add semantic UI react

--- my-app/src/Control/Control.js
+++ my-app/src/Control/Control.js
@@ -1,15 +1,69 @@
-import React from 'react';
+// import React from 'react';
+// import { Grid, Menu, Segment,Button } from 'semantic-ui-react'
+//
+// class Control extends React.Component {
+//   state = { activeItem: 'bio' }
+//
+//   handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+//
+//   render() {
+//     const { activeItem } = this.state
+//
+//     return (
+//       <div>
+//         <Button>
+//           HIHIHI
+//         </Button>
+//       {/* <Grid>
+//         <Grid.Column width={4}> */}
+//           <Menu fluid vertical tabular>
+//             <Menu.Item name='bio' active={activeItem === 'bio'} onClick={this.handleItemClick} />
+//             <Menu.Item name='pics' active={activeItem === 'pics'} onClick={this.handleItemClick} />
+//             <Menu.Item name='companies' active={activeItem === 'companies'} onClick={this.handleItemClick} />
+//             <Menu.Item name='links' active={activeItem === 'links'} onClick={this.handleItemClick} />
+//           </Menu>
+//         {/* </Grid.Column>
+//
+//         <Grid.Column stretched width={12}>
+//           <Segment>
+//             This is an stretched grid column. This segment will always match the tab height
+//           </Segment>
+//         </Grid.Column>
+//       </Grid> */}
+//     </div>
+//     )
+//   }
+// }
+//
+// export default Control;
+import React, { Component } from 'react'
+import { Grid, Menu, Segment } from 'semantic-ui-react'
 
-class Control extends React.Component {
-  constructor() {
-    super();
-    };
+export default class Control extends Component {
+  state = { activeItem: 'bio' }
 
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
 
   render() {
+    const { activeItem } = this.state
+
     return (
-      <div>Control</div>
+      <Grid>
+        <Grid.Column stretched width={12}>
+          <Segment>
+            This is an stretched grid column. This segment will always match the tab height
+          </Segment>
+        </Grid.Column>
+
+        <Grid.Column width={4}>
+          <Menu fluid vertical tabular='right'>
+            <Menu.Item name='bio' active={activeItem === 'bio'} onClick={this.handleItemClick} />
+            <Menu.Item name='pics' active={activeItem === 'pics'} onClick={this.handleItemClick} />
+            <Menu.Item name='companies' active={activeItem === 'companies'} onClick={this.handleItemClick} />
+            <Menu.Item name='links' active={activeItem === 'links'} onClick={this.handleItemClick} />
+          </Menu>
+        </Grid.Column>
+      </Grid>
     )
   }
 }
-export default Control;
--- my-app/src/Header/Header.js
+++ my-app/src/Header/Header.js
@@ -1,13 +1,32 @@
 import React from 'react';
-
+import gt from '../assets/images/gt.jpg'
+import nfls from '../assets/images/nfls.jpg'
 class Header extends React.Component {
+  styles= {
+     gtlogo: {
+       width: "200px",
+       height: "200px",
+       position: "absolute",
+       right: "20px",
+     },
+     nflslogo: {
+       width: "200px",
+       height: "200px",
+       position: "absolute",
+       left: "20px",
+
+     },
+   }
   constructor() {
     super();
   };
 
   render() {
     return (
-      <div>Header</div>
+    <div className="page-header">
+        <img src={gt} style={this.styles.gtlogo} className="gt-logo" alt="gt" />
+        <img src={nfls} style={this.styles.nflslogo} className='nfls-logo' alt='nfls'/>
+    </div>
     )
   }
 }
