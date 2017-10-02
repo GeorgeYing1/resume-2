a11c91f12814d8f2108b296046e81d594ba7e479
Ying, Jingya <Jingya.Ying@sage.com>
2017-10-01 21:31:46 -0400

    new branch with under construction logo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 //import Content from './Content/Content.js'
 import Control from './Control/Control.js'
+import { Image } from 'semantic-ui-react'
 import logo from './logo.svg';
 import './App.css';
+import UnderConstruction from './assets/images/under-construction.jpg';
 
 class App extends Component {
+  state = { 'showContruction': true }
+  constructionClick= () => {this.setState({'showContruction': false})}
+
   render() {
     return (
       <div className="App">
@@ -12,7 +17,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>George (Jingya) learning React</h2>
         </div>
-        <Control/>
+        { this.state.showContruction ? <Image label='My website is still under construction but welcome still! Click to Continue' src={UnderConstruction} onClick={this.constructionClick}/> : <Control/> }
 
       </div>
     );
